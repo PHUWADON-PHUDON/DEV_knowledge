793a12424575e5ee7dba7b82c4883cf7c117b2a3
PLUG <ppuwdon@gmail.com>
2022-12-04 18:59:46 +0700

    first commit

---
+++ knowlage/style.js
@@ -0,0 +1,10 @@
+window.alert("จำไว้นะไอ้หนูโปรแกรมรันไม่ได้ให้ใช้หลักการคนละตัวแปร :)");
+
+const Dark = document.querySelector(".dark");
+Dark.onclick=()=>{
+    const tap = document.querySelector("body")
+    tap.classList.toggle("dark2");
+    const Tap = document.querySelector("section");
+    Tap.classList.toggle("dark2");
+}
+
